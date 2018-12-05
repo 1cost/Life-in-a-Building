537703fb257c8d65ac9c3e9acb38e9a46faeec57
jakegerard <jake.a.gerard@gmail.com>
2018-12-05 11:21:08 -0800

    updating system

--- data/system.py
+++ data/system.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python
 
+import os
 import sys
+import signal
 import argparse
 import time
 import urllib
 import numpy as np
 import cv2
+
+loop = True
+
+def signalHandler(signal,frame):
+  global loop
+  print "Exiting Loop"
+  loop = False
 
 def getCurTime():
   t = time.localtime()
@@ -15,7 +24,18 @@ def getCurTime():
   return str(h+"_"+m+"_"+s)
 
 def collectData():
-  while True:
+  loc = "macd"
+  directory = getCurTime()
+  os.mkdir(directory)
+  print "Data stored in directory:", directory
+
+  count = 0
+  metafile = open(str(directory+"/meta.txt"),"w") 
+  metafile.write("Metadata for frame collection")
+  metafile.write("Location: "+str(loc))
+  metafile.write("Start Time: "+str(directory))
+
+  while loop:
     try:
       req = urllib.urlopen("http://10.17.166.21/axis-cgi/jpg/image.cgi")
       arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
@@ -23,14 +43,20 @@ def collectData():
 
       t = getCurTime()+".jpg"
       print t
-      cv2.imwrite(str(t),img)
+      cv2.imwrite(str(directory+"/"+t),img)
+      count+=1
     except:
       print "Error: Could not read in data. Attempting to reconnect."
       #exit(1)
 
     time.sleep(0.8)
+    
+  metafile.write("Total Frames: "+str(count))
+  metafile.close()
 
 # Main
+signal.signal(signal.SIGINT, signalHandler)
+
 feed = ""
 parser = argparse.ArgumentParser(description="Choose the operations to perform.")
 parser.add_argument("-c","--collect", nargs=1, dest="cname", help="Collect frames")
