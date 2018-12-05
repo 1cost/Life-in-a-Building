3c989eb3f92d5a3d9160658c838a14f256f4b4f5
jakegerard <jake.a.gerard@gmail.com>
2018-12-05 13:10:59 -0800

    updating system

--- data/system.py
+++ data/system.py
@@ -9,8 +9,10 @@ import urllib
 import numpy as np
 import cv2
 
-loop = True
+from ips import *
+#from videos import *
 
+loop = True
 def signalHandler(signal,frame):
   global loop
   print "Exiting Loop"
@@ -19,26 +21,30 @@ def signalHandler(signal,frame):
 def getCurTime():
   t = time.localtime()
   h = str(t.tm_hour)
+  h = h.rjust(2,"0")
   m = str(t.tm_min)
+  m = m.rjust(2,"0")
   s = str(t.tm_sec)
+  s = s.rjust(2,"0")
   return str(h+"_"+m+"_"+s)
 
-def collectData():
-  loc = "macd"
+def collectData(loc, ip):
+  signal.signal(signal.SIGINT, signalHandler)
   directory = getCurTime()
   os.mkdir(directory)
   print "Data stored in directory:", directory
 
   count = 0
   metafile = open(str(directory+"/meta.txt"),"w") 
-  metafile.write("Metadata for frame collection")
-  metafile.write("Location: "+str(loc))
-  metafile.write("Start Time: "+str(directory))
+  metafile.write("Metadata for frame collection\n")
+  metafile.write("Location: "+str(loc)+"\n")
+  metafile.write("Start Time: "+str(directory)+"\n")
 
   while loop:
     try:
-      req = urllib.urlopen("http://10.17.166.21/axis-cgi/jpg/image.cgi")
+      req = urllib.urlopen("http://"+ip+"/axis-cgi/jpg/image.cgi")
       arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
+      print(arr.shape)
       img = cv2.imdecode(arr, -1)
 
       t = getCurTime()+".jpg"
@@ -47,26 +53,32 @@ def collectData():
       count+=1
     except:
       print "Error: Could not read in data. Attempting to reconnect."
-      #exit(1)
+      exit(1)
 
     time.sleep(0.8)
-    
+
   metafile.write("Total Frames: "+str(count))
   metafile.close()
 
-# Main
-signal.signal(signal.SIGINT, signalHandler)
 
+
+##### Main #####
 feed = ""
 parser = argparse.ArgumentParser(description="Choose the operations to perform.")
+parser.add_argument("password", help="Insert database password")
 parser.add_argument("-c","--collect", nargs=1, dest="cname", help="Collect frames")
 parser.add_argument("-s","--system", nargs=1, dest="sname", help="Funnel to the system")
 
 args = parser.parse_args()
 
+ips = storeIPs(args.password)
+
 if args.cname:
-  print args.cname[0]
+  x = ipSwitch(args.cname[0])
+  collectData(args.cname[0], ips[x][1])
 elif args.sname:
   print args.sname[0]
-
-collectData()
+  for x in ips:
+    print x
+else:
+  print "Requires options"
