ec025781a50d04331f0db33a51933d1e5d1f532e
Sean Krasovic <m193480@usna.edu>
2019-03-05 16:04:32 -0500

    Script to run on pi to detect motion

---
+++ piScripts/motionSensor.py
@@ -0,0 +1,9 @@
+from gpiozero import MotionSensor
+import time
+import datetime
+pir = MotionSensor(4)
+
+def sendData(pir):
+    pir.wait_for_motion()
+    data = str(datetime.datetime.now())[:19]
+    return data
