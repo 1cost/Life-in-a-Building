25b606c45708a1a662fe1bbc6792553d50df7f85
Sean Krasovic <m193480@usna.edu>
2019-04-01 08:14:41 -0400

    Update motionSensor.py

--- piScripts/motionSensor.py
+++ piScripts/motionSensor.py
@@ -6,5 +6,6 @@ pir = MotionSensor(4)
 
 def sendData(pir):
     pir.wait_for_motion()
-    data = str(datetime.datetime.now())[:19]
+    time = str(datetime.datetime.now())[:19]
+    data = time[0:3] + time[5:6] + time[8:9] + time[11:12] + time[14:15] + time[17:18]
     return data
